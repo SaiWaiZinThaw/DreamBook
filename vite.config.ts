d08fb50819bc0f65584db9eb2ae2af5c000aeab0
Lucas Thaw <saiwaizinthaw@gmail.com>
2024-05-19 20:29:39 +0630

    updated setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
