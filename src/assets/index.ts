f0791771eb65e5092e597873ca3a61865ea14e58
Lucas Thaw <71580739+SaiWaiZinThaw@users.noreply.github.com>
2024-05-24 10:46:19 +0630

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -14,6 +14,7 @@ import Daisy from './daisy-pot-1.png';
 import LogoWhite from './Logo(White).png';
 import FooterImg from './Footer.png';
 import FooterLine from './FooterLine.png';
-import LibraryHero from './Library.png'
+import LibraryHero from './Library.png';
+import LoginBackground from "./LoginBackground.jpg";
 
-export {Logo, FooterLine, LogoWhite, FooterImg, LibraryHero , Animation, AppStore, GooglePLay, BestSelf, AuthBackground, About, LatestBg, LatestBooks, FeatureBg, Visit, HeroBg, Daisy}
+export {Logo, FooterLine, LogoWhite, FooterImg, LibraryHero , Animation, AppStore, GooglePLay, BestSelf, AuthBackground, About, LatestBg, LatestBooks, FeatureBg, Visit, HeroBg, Daisy, LoginBackground}
