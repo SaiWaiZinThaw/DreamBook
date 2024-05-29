6ac4c9ff0ad0aee395ff53e4c450c985ee6afd3e
Lucas Thaw <saiwaizinthaw@gmail.com>
2024-05-30 00:19:50 +0630

    update route

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { Route, Routes, useRoutes } from "react-router-dom";
 
-import { AuthRouter, UserRouter } from "./routers";
+import { AuthRouter, BookRouter, UserRouter } from "./routers";
 
 function App() {
   const UserRouting = useRoutes(UserRouter);
   const AuthRouting = useRoutes(AuthRouter);
+  const BookRouting = useRoutes(BookRouter);
 
   return (
     <div className="font-Inter">
       <Routes>
         <Route path="/*" element={UserRouting} />
         <Route path="/auth/*" element={AuthRouting} />
+      </Routes>
+      <Routes>
+        <Route path="/*" element={BookRouting} />
       </Routes>
     </div>
   );
