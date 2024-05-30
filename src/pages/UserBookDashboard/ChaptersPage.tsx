78a7207f9a8b096294722b857f88de3dc9fd01ba
thawzinhtut <thawzinhtut9421@gmail.com>
2024-05-29 23:03:00 -0700

    Chapters Upadate

--- src/pages/UserBookDashboard/ChaptersPage.tsx
+++ src/pages/UserBookDashboard/ChaptersPage.tsx
@@ -1,11 +1,9 @@
-import { BookDashBoardLayout } from "@/Layouts"
 import { Chapters } from "@/components/BookDashBoard"
 
 
 const ChaptersPage = () => {
   return (
     <div>
-        <BookDashBoardLayout/>
         <Chapters/>
     </div>
   )
