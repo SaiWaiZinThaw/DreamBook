de6fc560f3c858cc4375ef4ee0424b2fb937f9f9
Lucas Thaw <saiwaizinthaw@gmail.com>
2024-06-02 14:52:03 +0630

    remove unused item

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -1,6 +1,5 @@
 import { fetchMyProfile } from "@/api";
 import { useQuery } from "@tanstack/react-query";
-import { profileFetchData } from "@/types/types";
 
 export const useGetMe = () =>
   useQuery({
