bb51fe48dca8875a3ef07f1a39762b854706a71a
thawzinhtut <thawzinhtut9421@gmail.com>
2024-06-04 22:07:24 -0700

    Category API

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -37,6 +37,7 @@ const Login = () => {
     LoginAccount.mutate(data);
     console.log(data);
   };
+  
   return (
     <div className="flex flex-col items-center gap-10">
       <NavLink className="mt-0" to={"/"}>
