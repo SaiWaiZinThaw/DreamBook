6913bebb1985e68e233804920d0b2c1fa6238918
Lucsa Thaw <saiwaizinthaw@gmail.com>
2024-06-24 13:33:30 +0630

    fix conflicts and deployment errors

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,8 +2,5 @@ import Home from "./Home";
 import Library from "./Library";
 import CreateNewBook from "./CreateNewBook";
 import SignUp from "./auth/SignUp";
-import ChapterReading from "./ChapterReading";
 
-export {Home, Library, CreateNewBook, SignUp, ChapterReading};
-
-
+export { Home, Library, CreateNewBook, SignUp };
