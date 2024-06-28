1316cafe15cf9b5b02ca9272ea85810bb01a8f86
Lucsa Thaw <saiwaizinthaw@gmail.com>
2024-06-28 19:43:37 +0630

    fix errors

--- src/api/chapterProgressApi.tsx
+++ src/api/chapterProgressApi.tsx
@@ -1,48 +1,58 @@
 import { getToken } from "@/services/authService";
 import BaseURL from "../services/ApiEndPoint";
-import { ChapterProgressData, UpdateProgressData } from "@/types/types";
+import { ChapterProgressData } from "@/types/types";
 
 const token = getToken();
 
-export const createChapaterProgressApi = async ({ data }: { data: ChapterProgressData }) => {
-    const response: Response = await fetch(`${BaseURL}/chapter-progress`, {
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-        },
-        mode: "cors",
-        method: "POST",
-        redirect: "follow",
-        body: JSON.stringify(data),
-    });
-    const result = await response.json();
-    if (!response.ok) {
-      throw new Error(result.message);
-    }
-    return result;
-}
+export const createChapaterProgressApi = async ({
+  data,
+}: {
+  data: ChapterProgressData;
+}) => {
+  const response: Response = await fetch(`${BaseURL}/chapter-progress`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    mode: "cors",
+    method: "POST",
+    redirect: "follow",
+    body: JSON.stringify(data),
+  });
+  const result = await response.json();
+  if (!response.ok) {
+    throw new Error(result.message);
+  }
+  return result;
+};
 
-export const getCurrentChapter = async ({bookSlug}: {bookSlug:string}) => {
-    const response: Response = await fetch(`${BaseURL}/chapter-progress?slug=${bookSlug}`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          mode: "cors",
-          method: "GET",
-          redirect: "follow",
-    });
-    const result = await response.json();
-    if (!response.ok) {
-      throw new Error(result.message);
+export const getCurrentChapter = async ({ bookSlug }: { bookSlug: string }) => {
+  const response: Response = await fetch(
+    `${BaseURL}/chapter-progress?slug=${bookSlug}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      mode: "cors",
+      method: "GET",
+      redirect: "follow",
     }
-    return result;
-}
+  );
+  const result = await response.json();
+  if (!response.ok) {
+    throw new Error(result.message);
+  }
+  return result;
+};
 
-export const fetchProgressUpdate = async ( 
+export const fetchProgressUpdate = async (
   bookSlug: string,
-  data: { chapterId: number }) => {
-    const response : Response = await fetch(`${BaseURL}/chapter-progress/?slug=${bookSlug}`, {
+  data: { chapterId: number }
+) => {
+  const response: Response = await fetch(
+    `${BaseURL}/chapter-progress/?slug=${bookSlug}`,
+    {
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
@@ -52,10 +62,11 @@ export const fetchProgressUpdate = async (
       method: "PATCH",
       redirect: "follow",
       body: JSON.stringify(data),
-    });
-    const result = await response.json();
-    if (!response.ok) {
-      throw new Error(result.message);
     }
-    return result;
-}
+  );
+  const result = await response.json();
+  if (!response.ok) {
+    throw new Error(result.message);
+  }
+  return result;
+};
--- src/components/ChapterRead.tsx
+++ src/components/ChapterRead.tsx
@@ -1,13 +1,15 @@
-
-import { useState, useEffect } from 'react';
-import { useFetchAChapter, useFetchAllChapters } from '@/hooks/useFetchChapter';
-import { FaArrowLeft } from 'react-icons/fa';
-import { useNavigate, useParams } from 'react-router-dom';
-import DOMPurify from 'dompurify';
+import { useState, useEffect } from "react";
+import { useFetchAChapter, useFetchAllChapters } from "@/hooks/useFetchChapter";
+import { FaArrowLeft } from "react-icons/fa";
+import { useNavigate, useParams } from "react-router-dom";
+import DOMPurify from "dompurify";
 import { SlArrowRight } from "react-icons/sl";
 import { SlArrowLeft } from "react-icons/sl";
-import { useCreateChapterProgress, useFetchCurrentChapter, useUpdateChapterProgress } from '@/hooks/useChapterProgress';
-
+import {
+  useCreateChapterProgress,
+  useFetchCurrentChapter,
+  useUpdateChapterProgress,
+} from "@/hooks/useChapterProgress";
 
 const ChapterRead = () => {
   const { bookSlug, chapterId } = useParams<{
@@ -25,25 +27,24 @@ const ChapterRead = () => {
 
   const [activeChapterId, setActiveChapterId] = useState<number | null>(null);
   const createChapterProgress = useCreateChapterProgress();
-  const { data: getChapterProgress} = useFetchCurrentChapter(bookSlug!)
+  const { data: getChapterProgress } = useFetchCurrentChapter(bookSlug!);
   const updateProgress = useUpdateChapterProgress();
 
   useEffect(() => {
-    if(getChapterProgress?.chapterId) {
+    if (getChapterProgress?.chapterId) {
       setParsedChapterId(getChapterProgress.chapterId);
-      console.log(getChapterProgress.chapterId)
-      // navigate(`/book/${bookSlug}/chapters/${lastReadChapter}`);
+      console.log(getChapterProgress.chapterId);
     } else if (chapterId) {
-      setParsedChapterId(parseInt(chapterId, 10))
+      setParsedChapterId(parseInt(chapterId, 10));
     }
-  }, [getChapterProgress])
+  }, [getChapterProgress]);
 
   const handleChapterSelect = (id: number) => {
     navigate(`/book/${bookSlug}/chapter/${id}`);
     setParsedChapterId(id);
     setActiveChapterId(id);
 
-    if (getChapterProgress?.chapterId === id) {
+    if (getChapterProgress?.chapterId !== id) {
       updateProgress.mutate({ bookSlug: bookSlug!, data: { chapterId: id } });
     } else {
       createChapterProgress.mutate({ slug: bookSlug!, chapterId: id });
@@ -59,8 +60,10 @@ const ChapterRead = () => {
     }
   }, [updateProgress]);
 
-
-  const currentChapterIndex = getChapters?.findIndex((chapter:any) => chapter.chapterId === parsedChapterId) + 1;
+  const currentChapterIndex =
+    getChapters?.findIndex(
+      (chapter: any) => chapter.chapterId === parsedChapterId
+    ) + 1;
   const totalChapters = getChapters?.length;
 
   return (
@@ -93,42 +96,53 @@ const ChapterRead = () => {
         </ol>
       </div>
 
-
       <div className="flex flex-col w-screen min-h-screen">
         {isLoading && <p>Loading...</p>}
         {error && <p>Error loading chapter: {error.message}</p>}
         {getChapter && (
           <div className="ml-[108px] p-4">
-            <h1 className="mt-[28px] font-bold text-[36px] text-primary">{getChapter.title}</h1>
-            <div className="mt-[24px] font-normal text-lg" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(getChapter.content) }} />
-
+            <h1 className="mt-[28px] font-bold text-[36px] text-primary">
+              {getChapter.title}
+            </h1>
+            <div
+              className="mt-[24px] font-normal text-lg"
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(getChapter.content),
+              }}
+            />
           </div>
         )}
 
         <div className="flex justify-between mt-auto border border-t-slate-300">
-          <button 
-            onClick={()=> {
-              if(currentChapterIndex > 1) {
-                handleChapterSelect(getChapters[currentChapterIndex -2].chapterId);
-              }
-            }} 
-            disabled={currentChapterIndex <= 1}
-            className="flex justify-center items-center border-slate-300 my-[15.5px] border rounded-[8px] w-[113px] h-[42px]">
-              <SlArrowLeft className='mt-[2px] mr-2'/>
-              Previous
-          </button>
-
-          <div className="flex items-center">{currentChapterIndex} / {totalChapters}</div>
           <button
             onClick={() => {
-              if(currentChapterIndex < totalChapters) {
+              if (currentChapterIndex > 1) {
+                handleChapterSelect(
+                  getChapters[currentChapterIndex - 2].chapterId
+                );
+              }
+            }}
+            disabled={currentChapterIndex <= 1}
+            className="flex justify-center items-center border-slate-300 my-[15.5px] border rounded-[8px] w-[113px] h-[42px]"
+          >
+            <SlArrowLeft className="mt-[2px] mr-2" />
+            Previous
+          </button>
+
+          <div className="flex items-center">
+            {currentChapterIndex} / {totalChapters}
+          </div>
+          <button
+            onClick={() => {
+              if (currentChapterIndex < totalChapters) {
                 handleChapterSelect(getChapters[currentChapterIndex].chapterId);
               }
             }}
             disabled={currentChapterIndex >= totalChapters}
-            className="flex justify-center items-center bg-primary my-[15.5px] border rounded-[8px] w-[113px] h-[42px] text-slate-50">
-              Next 
-              <SlArrowRight className='mt-[2px] ml-2'/>
+            className="flex justify-center items-center bg-primary my-[15.5px] border rounded-[8px] w-[113px] h-[42px] text-slate-50"
+          >
+            Next
+            <SlArrowRight className="mt-[2px] ml-2" />
           </button>
         </div>
       </div>
