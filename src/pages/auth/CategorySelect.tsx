5734d2baebb217c82d75972312f22d2066438ad9
Lucsa Thaw <saiwaizinthaw@gmail.com>
2024-07-26 14:38:39 +0630

    fix deployment error

--- src/pages/auth/CategorySelect.tsx
+++ src/pages/auth/CategorySelect.tsx
@@ -1,13 +1,10 @@
 import { Button } from "@/components/ui/button";
-import { Loader2 } from "lucide-react";
-
 import {
   useFetchCategories,
   useInterestedCategories,
 } from "@/hooks/useFetchCategories";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Swal from "sweetalert2";
 
 interface Category {
   categoryId: string;
@@ -24,15 +21,8 @@ const CategorySelect = () => {
   }>({ categoryIds: [] });
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (selectedCategories.categoryIds.length < 3) {
-      setShowError(true);
-    } else {
-      setShowError(false);
-      interestedCategories.mutate(selectedCategories);
-    }
+    interestedCategories.mutate(selectedCategories);
   };
-  const [showError, setShowError] = useState(false);
-
 
   const handleCheckboxChange = (categoryId: string) => {
     setSelectedCategories((prevSelected) => {
@@ -56,27 +46,16 @@ const CategorySelect = () => {
     }
   }, [interestedCategories.isSuccess]);
 
-  useEffect(() => {
-    if (interestedCategories.isError) {
-      Swal.fire({
-        icon: "error",
-        title: "Error!",
-        text: "You need to select at least 3 categories",
-        timer: 2000,
-      });
-    }
-  }, [interestedCategories.isError]);
-
   return (
     <form
       onSubmit={handleSubmit}
-      className="gap-2 gap-x-3 md:gap-6 md:gap-x-8 grid grid-cols-2 py-4 w-full md:w-[600px]"
+      className="md:gap-6 gap-2 gap-x-3 md:gap-x-8 grid grid-cols-2 w-full md:w-[600px] py-4"
     >
       {!isLoading &&
         data?.map((category: Category) => (
           <div
             key={category.categoryId}
-            className="flex justify-between items-center bg-white px-4 py-2 rounded-[5px] h-[50px] md:h-auto"
+            className="flex justify-between items-center bg-white px-4 py-2 md:h-auto h-[50px] rounded-[5px]"
           >
             <div className="flex items-center gap-3">
               <input
@@ -89,13 +68,13 @@ const CategorySelect = () => {
                 )}
               />
               <label
-                className="flex items-center gap-3 font-bold font-Inter lg-text-lg text-[12px]"
+                className="flex items-center gap-3 text-[12px] font-bold lg-text-lg font-Inter"
                 htmlFor={category.categoryId}
               >
                 <img
                   src={category.icon}
                   alt={category.title}
-                  className="w-[20px] md:w-auto"
+                  className="md:w-auto w-[20px]"
                 />
                 {category.title}
               </label>
@@ -103,21 +82,9 @@ const CategorySelect = () => {
           </div>
         ))}
 
-      {!interestedCategories.isPending ? (
-        <Button className="col-span-2 mt-4" size={"full"} type="submit">
-          Finish
-        </Button>
-      ) : (
-        <Button
-          size={"full"}
-          disabled
-          className="flex items-center justify-center h-10 col-span-2 mt-4 md:h-auto"
-        >
-          <Loader2 className="h-4 mr-2 animate-spin" />
-          Please wait
-        </Button>
-      )}
-
+      <Button className="col-span-2 mt-4" size={"full"} type="submit">
+        Finish
+      </Button>
     </form>
   );
 };
